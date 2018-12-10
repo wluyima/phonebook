12187f2e3f9ea68a977a0c2ce9db01c6a5682b44
Wyclif Luyima <wyclif.luyima@gmail.com>
2018-12-10 15:20:17 -0500

    Added unit tests

--- src/main/java/com/phonebook/web/controller/PersonController.java
+++ src/main/java/com/phonebook/web/controller/PersonController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.phonebook.service.PersonService;
 
 @RestController
 @RequestMapping("/api/person")
+@CrossOrigin
 public class PersonController {
 	
 	private Logger log = LoggerFactory.getLogger(getClass());
---
+++ src/test/java/PersonControllerTest.java
@@ -0,0 +1,55 @@
+
+/*
+ * Add Copyright
+ */
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultActions;
+
+import com.phonebook.Bootstrap;
+import com.phonebook.model.Person;
+import com.phonebook.service.PersonService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Bootstrap.class)
+@AutoConfigureMockMvc
+@TestPropertySource("classpath:application-test.properties")
+public class PersonControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private PersonService service;
+	
+	private static final String SUB_PATH = "/api/person";
+	
+	@Test
+	public void getAll_shouldGetAllPersons() throws Exception {
+		Person p = new Person();
+		p.setFirstName("John");
+		p.setLastName("Doe");
+		p.setPhoneNumber("317-456-1233");
+		service.save(p);
+		
+		RequestBuilder request = get(SUB_PATH);
+		ResultActions resultActions = mockMvc.perform(request);
+		resultActions.andExpect(status().isOk());
+		resultActions.andExpect(jsonPath("length()", equalTo(1)));
+		
+	}
+	
+}
